df235ba5a82957e1d5292005d949163c7d890ed1
Art <mnsenna@protonmail.com>
2023-04-14 18:29:34 +1000

    Setup up script

---
+++ script.js
@@ -0,0 +1,14 @@
+const password = document.querySelector('#password');
+const passwordConfirm = document.querySelector('#password-confirm');
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const phone = document.querySelector('#phone-number');
+const passwordError = document.querySelector('#password-error');
+const passwordConfirmError = document.querySelector('#password-confirm-error');
+const firstNameError = document.querySelector('#first-name-error');
+const lastNameError = document.querySelector('#last-name-error');
+const emailError = document.querySelector('#email-error');
+const phoneError = document.querySelector('#phone-number-error');
+
+
